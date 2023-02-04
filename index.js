51207c4746d95d7e3f7c7bd7b27be12cb3840577
nikita <nikita.ratkevich91@mail.ru>
2023-02-04 16:51:44 +0200

    final

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const cors = require('cors');
 
 const token = '5939397200:AAH9rcFroMXSbWnuhTQcybx_NoIWt-rnAzs'
-const webAppUrl = 'https://1ab9-103-100-173-232.ap.ngrok.io/';
-const bot = new TelegramBot(token, {polling: true});
+const webAppUrl = 'https://fe8e-103-100-173-232.ap.ngrok.io';
 
+const bot = new TelegramBot(token, {polling: true});
+const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
 
     if(text === '/start') {
-        // await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
-        //     reply_markup: {
-        //         keyboard: [
-        //             [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
-        //         ]
-        //     }
-        // })
-
-        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
             reply_markup: {
-                inline_keyboard: [
-                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+                keyboard: [
+                    [{text: 'Заполнить форму', web_app: {url: webAppUrl}}]
                 ]
             }
         })
+
+        // await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+        //     reply_markup: {
+        //         inline_keyboard: [
+        //             [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+        //         ]
+        //     }
+        // })
     }
 
     if(msg?.web_app_data?.data) {
         try {
             const data = JSON.parse(msg?.web_app_data?.data)
             console.log(data)
-            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
-            await bot.sendMessage(chatId, 'Ваша страна: ');
-            await bot.sendMessage(chatId, 'Ваша улица: ' );
+            const info = data.productInfo.flatMap(item => ` ${item.title} - ${item.count} шт. `)
+            const message = `Ваш заказ: ${info}`
+
+            await bot.sendMessage(chatId, message)
+            await bot.sendMessage(chatId, `Способ оплаты: ${data.payment}`);
+            await bot.sendMessage(chatId, `Ваше местоположение: ${data.location}` );
 
             setTimeout(async () => {
                 await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
@@ -43,6 +52,27 @@ bot.on('message', async (msg) => {
         }
     }
 });
+
+app.post('/web-data', async (req, res) => {
+    const {queryId, productsInfo = [], payment, totalSum, location} = req.body;
+    const info = productsInfo.flatMap(item => ` ${item.title} - ${item.count} шт. `)
+    const message = `Ваш заказ: ${info}. На сумму ${totalSum}`
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            input_message_content: {
+                message_text: message
+            }
+        })
+        return res.status(200).json({});
+    } catch (e) {
+        return res.status(500).json({})
+    }
+})
+
+const PORT = 8000;
 
 
 
