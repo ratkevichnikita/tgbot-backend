b3cf28e9ecac37e24cca3f3d8032c1015de4b6c8
nikita <nikita.ratkevich91@mail.ru>
2023-02-13 04:25:10 +0000

    add some conditionals

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
-const token = '5939397200:AAH9rcFroMXSbWnuhTQcybx_NoIWt-rnAzs'
+const token = '6101662912:AAFqsGNZ6vrNq7tbkRwaNJPy0a4QsU9hyFg'
 const webAppUrl = 'https://ratkevichnikita.github.io/tgbot/';
 
 const bot = new TelegramBot(token, {polling: true});
@@ -19,47 +20,46 @@ bot.on('message', async (msg) => {
     const text = msg.text;
 
     if(text === '/start') {
-        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
-            reply_markup: {
-                keyboard: [
-                    [{text: 'Заполнить форму', web_app: {url: webAppUrl}}]
-                ]
-            }
-        })
-
-        // await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
-        //     reply_markup: {
-        //         inline_keyboard: [
-        //             [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
-        //         ]
-        //     }
-        // })
+        await bot.sendMessage(chatId, `Добрый день уважаемые друзья! В нашем магазине вы сможете найти развивающие товары, игрушки и книжки на русском языке для детей от 2 до 5 лет`)
+        await bot.sendMessage(chatId, `Для перехода в магазин, пожалуйста, нажмите синюю кнопку слева от строки ввода текста`)
+        await bot.sendMessage(chatId, `Если вдруг возникли технические сложности или любые другие уточняющие вопросы по нашему товару, просим вас написать @Juleera`)
     }
 
-    if(msg?.web_app_data?.data) {
-        try {
-            const data = JSON.parse(msg?.web_app_data?.data)
-            console.log(data)
-            const info = data.productInfo?.map(item => ` ${item.title} - ${item.count} шт. `).join('')
-            const message = `Ваш заказ: ${info}`
+    // if(msg?.web_app_data?.data) {
+    //     try {
+    //         const data = JSON.parse(msg?.web_app_data?.data)
+    //         console.log(data)
+    //         const info = data.productInfo?.map(item => ` ${item.title} - ${item.count} шт. `).join('')
+    //         const message = `Ваш заказ: ${info}`
 
-            await bot.sendMessage(chatId, message)
-            await bot.sendMessage(chatId, `Способ оплаты: ${data.payment}`);
-            await bot.sendMessage(chatId, `Ваше местоположение: ${data.location}` );
+    //         await bot.sendMessage(chatId, message)
+    //         await bot.sendMessage(chatId, `Способ оплаты: ${data.payment}`);
+    //         await bot.sendMessage(chatId, `Ваше местоположение: ${data.location}` );
 
-            setTimeout(async () => {
-                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
-            }, 3000)
-        } catch (e) {
-            console.log(e);
-        }
-    }
+    //         setTimeout(async () => {
+    //             await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+    //         }, 3000)
+    //     } catch (e) {
+    //         console.log(e);
+    //     }
+    // }
 });
 
+      
+
 app.post('/web-data', async (req, res) => {
-    const {queryId, productInfo = [], payment, totalSum, location} = req.body;
-    const info = productInfo.flatMap(item => ` ${item.title} - ${item.count} шт. `)
-    const message = `Ваш заказ: ${info}. На сумму ${totalSum}`
+    const {queryId, productInfo = [], payment, totalSum, location, userName} = req.body;
+    const info = productInfo.flatMap(item => ` ${item.title} - ${item.count} шт. `);
+    const message = `Ваш заказ: ${info}. На сумму ${totalSum}. Способ оплаты: ${payment}. Локация: ${location}.`
+
+    const message_admin = `Ваш заказ: ${info}. 
+    На сумму ${totalSum}. \n
+    Способ оплаты: ${payment}. \n
+    Локация: ${location}. \n
+    Пользователь: ${userName}
+    `
+    axios.post(`https://api.telegram.org/bot${token}/sendMessage?chat_id=@smart_seeds_toys&text=${message_admin}`)
+    
     try {
         await bot.answerWebAppQuery(queryId, {
             type: 'article',
@@ -69,6 +69,7 @@ app.post('/web-data', async (req, res) => {
                 message_text: message
             }
         })
+        
         return res.status(200).json({});
     } catch (e) {
         return res.status(500).json({})
